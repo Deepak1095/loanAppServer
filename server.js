10f76fc59bea636de3963c8a780844faef6e7c86
Deepak1095 <ddhankhar999@gmail.com>
2023-07-19 21:35:52 +0530

    add cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 const cors=require("cors")
-server.use(cors)
+server.use(cors())
 server.use(middlewares);
 server.use(router);
 
